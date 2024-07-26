2708bf0eb066a5060e10ca47da7175a3f8287294
arceoh <arceoh@gmail.com>
2024-07-25 20:34:43 -0700

    Styling alphabet

--- src/components/Aurebesh/Aurebesh.js
+++ src/components/Aurebesh/Aurebesh.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { AUREBESH } from './AurebeshAlphabet';
 
 const Aurebesh = () => {
-  const keys = Object.keys(AUREBESH);
+	const keys = Object.keys(AUREBESH);
 
-  const svgComponents = keys.map((key) => {
-    const { icon: SvgComponent, string } = AUREBESH[key];
-    return (
-      <div key={key} className="p-2 text-center border-2 border-slate-100"> <SvgComponent className="w-6 h-6" />
-        <span>{string}</span></div>
-    );
-  });
+	const svgComponents = keys.map((key) => {
+		const { icon: SvgComponent, string } = AUREBESH[key];
+		return (
+			<div key={key} className="p-2 text-center border-2 rounded-sm border-neutral-200">
+				{' '}
+				<SvgComponent className="w-6 h-6" />
+				<span>{string}</span>
+			</div>
+		);
+	});
 
-  return <div>
-    <h2>Aurebesh Alphabet</h2>
-    <div className='flex flex-wrap gap-2'>{svgComponents}</div>
-  </div>;
-}
+	return (
+		<div>
+			<h2>Aurebesh Alphabet</h2>
+			<div className="flex flex-wrap gap-2">{svgComponents}</div>
+		</div>
+	);
+};
 
 export default Aurebesh;
