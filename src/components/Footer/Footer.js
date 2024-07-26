6854bc1ca0fbbce21b4f0d2bb1f97e16382a5ba6
arceoh <arceoh@gmail.com>
2024-07-25 20:33:16 -0700

    Footer component with class info.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<div className="bg-primary">
+			<div className="container py-8 mx-auto text-primary-content">
+				<p className="text-lg font-bold">Created for SBCCD Discrete Structures-130-30 (CS-130-30) Summer 2024</p>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
