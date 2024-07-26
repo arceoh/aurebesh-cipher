c9f31c6abb5003b22daca0f74cad2e97bf734217
arceoh <arceoh@gmail.com>
2024-07-25 20:37:59 -0700

    Styling

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 function Head() {
-  return (
-    <div className="bg-blue-200">
-      <div className="container mx-auto navbar">
-        <h1 className="text-2xl btn btn-ghost">Aurebesh Cipher</h1>
-      </div>
-    </div>
-  );
+	return (
+		<div className="mb-4 bg-neutral text-neutral-content">
+			<div className="container mx-auto navbar">
+				<h1 className="text-2xl font-black">Aurebesh Cipher</h1>
+			</div>
+		</div>
+	);
 }
 
 export default Head;
